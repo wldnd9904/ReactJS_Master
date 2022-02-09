d20f774f3262e786096a9db5d23ca03b4d9ca193
wldnd9904 <wldnd9904@uos.ac.kr>
2022-02-09 21:50:46 +0900

    styled components 2

--- react-masterclass/src/App.js
+++ react-masterclass/src/App.js
@@ -4,27 +4,37 @@ const Father = styled.div`
   display: flex;
 `;
 
-const BoxOne = styled.div`
-  background-color: teal;
+const Box = styled.div`
+  background-color: ${(props) => props.bgColor};
   width: 100px;
   height: 100px;
 `;
-const BoxTwo = styled.div`
-  background-color: tomato;
-  width: 100px;
-  height: 100px;
+//extension
+const Circle = styled(Box)`
+  border-radius: 50px;
 `;
 const Text = styled.span`
   color: white;
 `;
-
+const Btn = styled.button`
+  color: white;
+  background-color: tomato;
+  border: 0;
+  border-radius: 15px;
+`;
+const Input = styled.input.attrs({ required: true, minLength: 10 })`
+  background-color: tomato;
+`;
 function App() {
   return (
-    <Father>
-      <BoxOne>
-        <Text>Hello</Text>
-      </BoxOne>
-      <BoxTwo />
+    <Father as="header">
+      <Input />
+      <Input />
+      <Input />
+      <Input />
+      <Input />
+      <Input />
+      <Input />
     </Father>
   );
 }
