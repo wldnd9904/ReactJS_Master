a96c98577ef948f254d2d9f85b94df4af1a196b4
wldnd9904 <wldnd9904@uos.ac.kr>
2022-02-25 11:55:52 +0900

    .

--- typescript-app/src/App.tsx
+++ typescript-app/src/App.tsx
@@ -5,8 +5,8 @@ import Circle from "./Circle";
 function App() {
   return ( 
     <div>
-      <Circle bgColor="teal" radius={200}/>
-      <Circle bgColor="tomato" radius={100}/>
+      <Circle bgColor="teal" radius={200} borderColor="black"/>
+      <Circle bgColor="tomato" radius={100} text="안녕"/>
     </div>
   );
 }
--- typescript-app/src/Circle.tsx
+++ typescript-app/src/Circle.tsx
@@ -1,24 +1,34 @@
+import { useState } from "react";
 import styled from "styled-components";
 
 const Container = styled.div<ContainerProps>`
-    width:${props=>props.radius}px;
-    height:${props=>props.radius}px;
+    width:${props=>props.radius*2}px;
+    height:${props=>props.radius*2}px;
     background-color: ${props=>props.bgColor};
-    border-radius:${props=>props.radius/2}px;
+    border-radius:${props=>props.radius}px;
+    border:3px solid ${props => props.borderColor};
+    display: flex;
+    justify-content: center;
+    align-items: center;
 `;
 
 interface ContainerProps {
-    bgColor: string;
+    bgColor:string;
+    borderColor:string;
     radius:number;
+    text?:string;
 }
-
 interface CircleProps {
     bgColor:string;
+    borderColor?:string;
     radius:number;
+    text?:string;
 }
 
-function Circle({bgColor, radius}: CircleProps) {
-    return <Container radius={radius} bgColor={bgColor}/>;
+function Circle({bgColor, radius, borderColor, text = "default text"}: CircleProps) {
+    const [counter, setCounter] = useState(1);
+    setCounter(2)
+    return <Container radius={radius} bgColor={bgColor} borderColor={borderColor ?? bgColor}>{text}</Container>;
 }
 
 export default Circle;
