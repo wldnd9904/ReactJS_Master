f41c3f8af9520a2e025e7436232f914b0faf2759
wldnd9904 <wldnd9904@uos.ac.kr>
2022-09-20 23:52:39 +0900

    #3.3 Optional Props

---
+++ react-typescript/src/App.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+import Circle from "./Circle";
+
+function App() {
+  return (
+    <div>
+      <Circle borderColor="red" bgColor="teal"/>
+      <Circle bgColor="tomato"/>
+    </div>
+  );
+}
+
+export default App;
