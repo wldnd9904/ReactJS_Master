e59c36a19857a60cb6b2fa7a03e18a9ee1671b93
wldnd9904 <wldnd9904@uos.ac.kr>
2022-09-22 05:40:51 +0900

    #4.4 Route States

--- react-crypto-tracker/src/routes/Coin.tsx
+++ react-crypto-tracker/src/routes/Coin.tsx
@@ -1,7 +1,48 @@
+import styled from "styled-components";
 import {useParams} from "react-router";
+import { useState } from "react";
+import { useLocation } from "react-router-dom";
+
+const Container = styled.div`
+    padding: 0px 20px;
+    max-width:480px;
+    margin:0 auto;
+`;
+
+const Header = styled.header`
+    height:10vh;
+    display:flex;
+    justify-content:center;
+    align-items: center;
+`;
+
+const Title = styled.h1`
+    font-size:40px;
+    color:${props=>props.theme.accentColor};
+`;
+
+const Loader = styled.span`
+    text-align:center;
+    display:block;
+`;
+
+interface RouteState{
+    name:string;
+}
 
 function Coin(){
+    const [loading, setLoading] = useState<boolean>(true);
     const {coinId} = useParams();
-    return <h1>Coin: {coinId}</h1>
+    const location = useLocation();
+    const state = location.state as RouteState;
+    return (    <Container>
+    <Header>
+        <Title>Coin {state?.name || "loading..."}</Title>
+    </Header>
+    {loading?
+    <Loader>
+        "Loading..."
+    </Loader>
+    :null}</Container>);
 }
 export default Coin;
