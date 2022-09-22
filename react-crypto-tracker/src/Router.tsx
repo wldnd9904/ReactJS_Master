318b91a29b1fe11eea3a25b00e14def2622a62e2
wldnd9904 <wldnd9904@uos.ac.kr>
2022-09-22 16:42:01 +0900

    #3.7 Nested Routes part One

--- react-crypto-tracker/src/Router.tsx
+++ react-crypto-tracker/src/Router.tsx
@@ -5,14 +5,8 @@ import Coins from "./routes/Coins"
 function Router() {
     return <BrowserRouter>
     <Routes>
-        <Route path="/:coinId" element={
-            <Coin/>
-            }>
-        </Route>
-        <Route path="/" element={
-            <Coins/>
-            }>
-        </Route>
+        <Route path="/:coinId/*" element={<Coin/>}/>
+        <Route path="/" element={<Coins/>}/>
     </Routes>
     </BrowserRouter>
 }
--- react-crypto-tracker/src/routes/Coin.tsx
+++ react-crypto-tracker/src/routes/Coin.tsx
@@ -1,7 +1,10 @@
 import styled from "styled-components";
 import {useParams} from "react-router";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
+import Chart from "../routes/Chart";
+import Price from "../routes/Price";
+import {Routes, Route} from "react-router-dom";
 
 const Container = styled.div`
     padding: 0px 20px;
@@ -25,24 +28,148 @@ const Loader = styled.span`
     text-align:center;
     display:block;
 `;
+const Overview = styled.div`
+   display: flex;
+   justify-content: space-between;
+   background-color: rgba(0, 0, 0, 0.5);
+   padding: 10px 20px;
+   border-radius: 10px;
+ `;
+ const OverviewItem = styled.div`
+   display: flex;
+   flex-direction: column;
+   align-items: center;
+
+   span:first-child {
+     font-size: 10px;
+     font-weight: 400;
+     text-transform: uppercase;
+     margin-bottom: 5px;
+   }
+ `;
+ const Description = styled.p`
+   margin: 20px 0px;
+ `;
 
 interface RouteState{
     name:string;
 }
 
+interface IInfo{
+    id: string;
+    name: string;
+    symbol: string;
+    rank: number;
+    is_new: boolean;
+    is_active: boolean;
+    type: string;
+    logo: string;
+    description: string;
+    message: string;
+    open_source: boolean;
+    started_at: string;
+    development_status: string;
+    hardware_wallet: boolean;
+    proof_type: string;
+    org_structure: string;
+    hash_algorithm: string;
+    first_data_at: string;
+    last_data_at: string;
+}
+
+interface IQuote{
+price    :number;
+volume_24h:number;
+volume_24h_change_24h:number;
+market_cap:number;
+market_cap_change_24h:number;
+percent_change_15m:number;
+percent_change_30m:number;
+percent_change_1h:number;
+percent_change_6h:number;
+percent_change_12h:number;
+percent_change_24h:number;
+percent_change_7d:number;
+percent_change_30d:number;
+percent_change_1y:number;
+ath_price:number;
+ath_date:string;
+percent_from_price_ath:number;
+}
+
+interface IPrice{
+    id: string;
+    name: string;
+    symbol: string;
+    rank: number;
+    circulating_supply: number;
+    total_supply: number;
+    max_supply: number;
+    beta_value: number;
+    first_data_at: string;
+    last_updated: string;
+    quotes: {
+        USD: IQuote;
+    }
+}
+
 function Coin(){
-    const [loading, setLoading] = useState<boolean>(true);
+    const [loading,setLoading] = useState<boolean>(true);
     const {coinId} = useParams();
     const location = useLocation();
     const state = location.state as RouteState;
-    return (    <Container>
-    <Header>
-        <Title>Coin {state?.name || "loading..."}</Title>
-    </Header>
-    {loading?
-    <Loader>
-        "Loading..."
-    </Loader>
-    :null}</Container>);
+    const [info, setInfo] = useState<IInfo>();
+    const [priceInfo, setPriceInfo] = useState<IPrice>();
+    useEffect(()=>{
+        (async () => {
+            const infoData = await (await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)).json();
+            const priceData = await (await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)).json();
+            setInfo(infoData);
+            setPriceInfo(priceData);
+            setLoading(false);
+        })();
+    },[coinId]);
+    return (
+     <Container>
+        <Header>
+            <Title>Coin {state?.name? state.name: loading? "Loading..." : info?.name}</Title>
+        </Header>
+        {loading ? (
+         <Loader>Loading...</Loader>
+       ) : (
+         <>
+           <Overview>
+             <OverviewItem>
+               <span>Rank:</span>
+               <span>{info?.rank}</span>
+             </OverviewItem>
+             <OverviewItem>
+               <span>Symbol:</span>
+               <span>${info?.symbol}</span>
+             </OverviewItem>
+             <OverviewItem>
+               <span>Open Source:</span>
+               <span>{info?.open_source ? "Yes" : "No"}</span>
+             </OverviewItem>
+           </Overview>
+           <Description>{info?.description}</Description>
+           <Overview>
+             <OverviewItem>
+               <span>Total Suply:</span>
+               <span>{priceInfo?.total_supply}</span>
+             </OverviewItem>
+             <OverviewItem>
+               <span>Max Supply:</span>
+               <span>{priceInfo?.max_supply}</span>
+             </OverviewItem>
+           </Overview>
+           <Routes>
+             <Route path={`/${coinId}/price`} element={<Price />} />
+             <Route path={`/${coinId}/chart`} element={<Chart />} />
+           </Routes>
+         </>
+       )}
+    </Container>
+    );
 }
 export default Coin;
