31901e1dd2c2a171490e54773c3fe60d2ee32999
wldnd9904 <wldnd9904@uos.ac.kr>
2022-10-06 23:19:53 +0900

    #4.10 React Query part Two

--- react-crypto-tracker/src/routes/Coin.tsx
+++ react-crypto-tracker/src/routes/Coin.tsx
@@ -6,25 +6,24 @@ import Chart from "../routes/Chart";
 import Price from "../routes/Price";
 import {Routes, Route, useMatch } from "react-router-dom";
 import { Link } from "react-router-dom";
+import { useQuery } from "react-query";
+import { fetchCoinInfo, fetchCoinTickers } from "./api";
 
 const Container = styled.div`
     padding: 0px 20px;
     max-width:480px;
     margin:0 auto;
 `;
-
 const Header = styled.header`
     height:10vh;
     display:flex;
     justify-content:center;
     align-items: center;
 `;
-
 const Title = styled.h1`
     font-size:40px;
     color:${props=>props.theme.accentColor};
 `;
-
 const Loader = styled.span`
     text-align:center;
     display:block;
@@ -57,7 +56,6 @@ grid-template-columns: repeat(2, 1fr);
 margin: 25px 0px;
 gap: 10px;
 `;
-
 const Tab = styled.span<{ isActive: boolean }>`
 text-align: center;
 text-transform: uppercase;
@@ -72,11 +70,9 @@ a {
   display: block;
 }
 `;
-
 interface RouteState{
     name:string;
 }
-
 interface IInfo{
     id: string;
     name: string;
@@ -98,7 +94,6 @@ interface IInfo{
     first_data_at: string;
     last_data_at: string;
 }
-
 interface IQuote{
 price    :number;
 volume_24h:number;
@@ -118,7 +113,6 @@ ath_price:number;
 ath_date:string;
 percent_from_price_ath:number;
 }
-
 interface IPrice{
     id: string;
     name: string;
@@ -134,29 +128,31 @@ interface IPrice{
         USD: IQuote;
     }
 }
-
 function Coin(){
-    const [loading,setLoading] = useState<boolean>(true);
-    const {coinId} = useParams();
-    const location = useLocation();
-    const state = location.state as RouteState;
-    const [info, setInfo] = useState<IInfo>();
-    const [priceInfo, setPriceInfo] = useState<IPrice>();
-    const priceMatch = useMatch("/:coinId/price");
-    const chartMatch = useMatch("/:coinId/chart");
-    useEffect(()=>{
-        (async () => {
-            const infoData = await (await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)).json();
-            const priceData = await (await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)).json();
-            setInfo(infoData);
-            setPriceInfo(priceData);
-            setLoading(false);
-        })();
-    },[coinId]);
+  const {coinId} = useParams();
+  const location = useLocation();
+  const state = location.state as RouteState;
+  const priceMatch = useMatch("/:coinId/price");
+  const chartMatch = useMatch("/:coinId/chart");
+  const {isLoading: infoLoading, data: infoData} = useQuery<IInfo>(["info",coinId], () => fetchCoinInfo(coinId));
+  const {isLoading: tickersLoading, data: tickersData} = useQuery<IPrice>(["tickers",coinId], () => fetchCoinTickers(coinId));
+  //const [loading,setLoading] = useState<boolean>(true);
+  //const [priceInfo, setPriceInfo] = useState<IPrice>();
+  //const [info, setInfo] = useState<IInfo>();
+  //  useEffect(()=>{
+  //      (async () => {
+  //          const infoData = await (await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)).json();
+  //          const priceData = await (await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)).json();
+  //          setInfo(infoData);
+  //          setPriceInfo(priceData);
+  //          setLoading(false);
+  //      })();
+  //  },[coinId]);
+  const loading = infoLoading || tickersLoading;
     return (
      <Container>
         <Header>
-            <Title>{state?.name? state.name: loading? "Loading..." : info?.name}</Title>
+            <Title>{state?.name? state.name: loading? "Loading..." : infoData?.name}</Title>
         </Header>
         {loading ? (
          <Loader>Loading...</Loader>
@@ -165,26 +161,26 @@ function Coin(){
            <Overview>
              <OverviewItem>
                <span>Rank:</span>
-               <span>{info?.rank}</span>
+               <span>{infoData?.rank}</span>
              </OverviewItem>
              <OverviewItem>
                <span>Symbol:</span>
-               <span>${info?.symbol}</span>
+               <span>${infoData?.symbol}</span>
              </OverviewItem>
              <OverviewItem>
                <span>Open Source:</span>
-               <span>{info?.open_source ? "Yes" : "No"}</span>
+               <span>{infoData?.open_source ? "Yes" : "No"}</span>
              </OverviewItem>
            </Overview>
-           <Description>{info?.description}</Description>
+           <Description>{infoData?.description}</Description>
            <Overview>
              <OverviewItem>
                <span>Total Suply:</span>
-               <span>{priceInfo?.total_supply}</span>
+               <span>{tickersData?.total_supply}</span>
              </OverviewItem>
              <OverviewItem>
                <span>Max Supply:</span>
-               <span>{priceInfo?.max_supply}</span>
+               <span>{tickersData?.max_supply}</span>
              </OverviewItem>
            </Overview>
            <Tabs>
--- react-crypto-tracker/src/routes/api.ts
+++ react-crypto-tracker/src/routes/api.ts
@@ -1,3 +1,13 @@
-export async function fetchCoins() {
-    return fetch("https://api.coinpaprika.com/v1/coins").then((result)=>result.json());
+const BASE_URL = `https://api.coinpaprika.com/v1`;
+
+export function fetchCoins() {
+    return fetch(`${BASE_URL}/coins`).then((result)=>result.json()).then((json)=>json.slice(0,30));
+}
+
+export function fetchCoinInfo(coinId:string|undefined){
+    return fetch(`${BASE_URL}/coins/${coinId}`).then((result)=>result.json());
+}
+
+export function fetchCoinTickers(coinId:string|undefined){
+    return fetch(`${BASE_URL}/tickers/${coinId}`).then((result)=>result.json());
 }
